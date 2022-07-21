3c7802957c3f6f65d3e6d66c06aa73d63f0142ab
Aleksandr_Kryvobok <krivobok.olexandr@gmail.com>
2022-07-21 13:40:47 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const todoForm = document.getElementById('toto-form');
+const addImput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
